23c5eb186a564b03fcc7d9a202d1b0261cca44c2
Davidheca2006 <davidheca2p@gmail.com>
2024-12-02 21:26:39 -0600

    1

---
+++ src/proyectoprogra/JuegoSupermercado.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyectoprogra;
+
+/**
+ *
+ * @author da_he
+ */
+public class JuegoSupermercado {
+    
+    private Producto[] productos;
+    private CarritoDeCompras carrito;
+    private Producto[] listaMama;
+    private double dineroJugador;
+    
+}
---
+++ src/proyectoprogra/ProyectoProgra.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyectoprogra;
+
+/**
+ *
+ * @author da_he
+ */
+public class ProyectoProgra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Inicio inicio = new Inicio();
+        inicio.setVisible(true);
+        inicio.setLocationRelativeTo(null);
+    }
+    
+}
